c942b293e78d2b0793c88981fd537ea49652559b
ikhan-123 <ifficodder@gmail.com>
2025-02-04 02:15:57 +0500

    currency converter

---
+++ currencyconvertor/postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: /d:/Chai-aur-React/currencyconvertor/postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ currencyconvertor/tailwind.config.js
@@ -0,0 +1,11 @@
+// filepath: /d:/Chai-aur-React/currencyconvertor/tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
